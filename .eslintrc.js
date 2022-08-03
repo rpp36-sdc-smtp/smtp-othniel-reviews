b662e255871a437c73ee80b83aeac1a9eea3cec6
Othniel Chan <othnielhr.chan@gmail.com>
2022-08-03 13:37:00 -0500

    (init)

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true,
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
