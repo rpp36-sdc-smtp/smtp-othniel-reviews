207f8b3b40a0f080c24909211723ff8b51758d11
Othniel Chan <othnielhr.chan@gmail.com>
2022-08-16 16:33:46 -0500

    changed listen location

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 // var router = require('./routes.js');
 
@@ -8,10 +7,6 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.sendStatus(200);
-});
-
-app.listen(port, () => {
-  console.log('listening on port ', port);
 });
 
 module.exports = app;
