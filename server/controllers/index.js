6970d8846ac6daf66e2eb67a51e1ae57a60d0985
Othniel Chan <othnielhr.chan@gmail.com>
2022-09-10 16:13:03 -0500

    tests

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,8 +7,8 @@ module.exports = {
       if (req.query.sort === 'newest') {
         orderBy = 'order by date desc';
       }
-      if (!req.query.product_id) {
-        res.send('Error: invalid product_id provided');
+      if (!req.query.product_id || !parseInt(req.query.product_id)) {
+        res.status(400).send('Error: invalid product_id provided');
       } else {
         let { product_id } = req.query;
         let result = {
@@ -45,51 +45,53 @@ module.exports = {
       }
     },
     post: function (req, res) {
-      let submit = req.body;
-      let { photos, characteristics } = submit;
-      let result = {};
-      pool.query('select id from reviews order by id desc limit 1')
-        .then((data) => {
-          const revQuery = {
-            text: 'insert into reviews (id, reviewer_name, reviewer_email, rating, summary, recommend, body, date, product_id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id',
-            values: [data.rows[0].id + 1, submit.name, submit.email, Number(submit.rating), submit.summary, Boolean(submit.recommend), submit.body, Date.now(), submit.product_id],
-          };
-          const charQuery = {
-            text: 'insert into characteristicreview (id, characteristic_id, review_id, value) values',
-          };
-          const photoQuery = {
-            text: 'insert into photos (id, url, review_id) values',
-          };
-          pool.query(revQuery)
-            .then(async (data) => {
-              result.review_id = data.rows[0].id;
-              let count = 1;
-              for (const [key, value] of Object.entries(submit.characteristics)) {
-                let newId = await pool.query('select max(id) from characteristicreview');
-                charQuery.text += ` (${newId.rows[0].max + count}, ${key}, ${result.review_id}, ${value}),`;
-                count++;
-              }
-              charQuery.text = charQuery.text.substring(0, charQuery.text.length - 1);
-              console.log(charQuery.text);
-              await pool.query(charQuery.text);
-              if (photos.length > 0) {
-                let newId = await pool.query('select max(id) from photos');
-                count = 1;
-                photos.forEach(photo => {
-                  photoQuery.text += `(${newId.rows[0].max + count}, '${photo}', ${result.review_id}),`;
+      if (!parseInt(req.body.product_id)) {
+        res.send(400);
+      } else {
+        let submit = req.body;
+        let { photos, characteristics } = submit;
+        let result = {};
+        pool.query('select id from reviews order by id desc limit 1')
+          .then((data) => {
+            const revQuery = {
+              text: 'insert into reviews (id, reviewer_name, reviewer_email, rating, summary, recommend, body, date, product_id) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id',
+              values: [data.rows[0].id + 1, submit.name, submit.email, Number(submit.rating), submit.summary, Boolean(submit.recommend), submit.body, Date.now(), submit.product_id],
+            };
+            const charQuery = {
+              text: 'insert into characteristicreview (id, characteristic_id, review_id, value) values',
+            };
+            const photoQuery = {
+              text: 'insert into photos (id, url, review_id) values',
+            };
+            pool.query(revQuery)
+              .then(async (data) => {
+                result.review_id = data.rows[0].id;
+                let count = 1;
+                for (const [key, value] of Object.entries(submit.characteristics)) {
+                  let newId = await pool.query('select max(id) from characteristicreview');
+                  charQuery.text += ` (${newId.rows[0].max + count}, ${key}, ${result.review_id}, ${value}),`;
                   count++;
-                  console.log(photoQuery.text);
-                });
-                photoQuery.text = photoQuery.text.substring(0, photoQuery.text.length - 1);
-                await pool.query(photoQuery.text);
+                }
+                charQuery.text = charQuery.text.substring(0, charQuery.text.length - 1);
+                await pool.query(charQuery.text);
+                if (photos.length > 0) {
+                  let newId = await pool.query('select max(id) from photos');
+                  count = 1;
+                  photos.forEach(photo => {
+                    photoQuery.text += `(${newId.rows[0].max + count}, '${photo}', ${result.review_id}),`;
+                    count++;
+                  });
+                  photoQuery.text = photoQuery.text.substring(0, photoQuery.text.length - 1);
+                  await pool.query(photoQuery.text);
 
-              }
-              res.status(201).send('Created');
-            });
-        }).catch((err) => {
-          console.log(err);
-          res.status(500).send(err);
-        });
+                }
+                res.status(201).send('Created');
+              });
+          }).catch((err) => {
+            console.log(err);
+            res.status(500).send(err);
+          });
+      }
     },
     helpful: function (req, res) {
       let { review_id } = req.params;
@@ -128,7 +130,6 @@ module.exports = {
       Promise.all([rating, rec, char])
         .then((result) => {
           let [rating, recommend, char] = result;
-          console.log(char.rows);
           rating = format(rating.rows);
           recommend = format(recommend.rows);
           char = charFormat(char.rows);
--- server/serverSpec/server.test.js
+++ server/serverSpec/server.test.js
@@ -1,9 +1,97 @@
 const server = require('../server.js');
 const req = require('supertest');
+const pool = require('../../database/index.js');
 
 describe('Server responses', () => {
   it('should respond with a status code of 200 at /', async () => {
-    var res = await req(server).get('/');
+    const res = await req(server).get('/');
     expect(res.status).toEqual(200);
+  });
+});
+
+describe('Get routes for reviews', () => {
+  it('Should get the correct reviews from the given product id', async () => {
+    const res = await req(server).get('/reviews?product_id=1000011');
+    console.log(res);
+    expect(res.body.product).toEqual('1000011');
+  });
+
+  it('Should contain a photos array within the reviews object', async () => {
+    const res = await req(server).get('/reviews?product_id=1000011');
+    expect(Array.isArray(res.body.results[0].photos)).toBeTruthy();
+  });
+
+  it('Should have default page and count of 0 and 5 respectively', async () => {
+    const res = await req(server).get('/reviews?product_id=1000011');
+    // console.log(res.body);
+    expect(res.body.page).toEqual(0);
+    expect(res.body.count).toEqual(5);
+  });
+
+  it('Should sort results by date', async () => {
+    const res = await req(server).get('/reviews?product_id=1000011&sort=newest');
+    let i = 0;
+    while(i > res.body.results.length - 1) {
+      expect(res.body.results[i].date > res.body.results[i + 1].date).toBeTruthy();
+      i++;
+    }
+  });
+
+  it('Should sort results by helpfulness', async () => {
+    const res = await req(server).get('/reviews?product_id=1000011&sort=helpfulness');
+    let i = 0;
+    while(i > res.body.results.length - 1) {
+      expect(res.body.results[i].helpfulness >= res.body.results[i + 1].helpfulness).toBeTruthy();
+      i++;
+    }
+  });
+
+  it('Should return an empty results array if there are no reviews', async () => {
+    const res = await req(server).get('/reviews?product_id=1000012');
+    expect(Array.isArray(res.body.results)).toBeTruthy();
+    expect(res.body.results.length).toEqual(0);
+  });
+
+  it('Should respond with a 400 error with a bad request', async () => {
+    const res = await req(server).get('/reviews?product_id=qwerty');
+    expect(res.status).toEqual(400);
+  });
+});
+
+describe('Post routes for reviews', () => {
+  const body = {
+    'product_id': 1000011,
+    'rating':  4,
+    'summary': 'summary here',
+    'body': 'body here',
+    'recommend': true,
+    'name': 'test',
+    'email': 'test@tester.com',
+    'photos': ['https://res.cloudinary.com/lexicon-atelier/image/upload/v1661733999/nnoyb1vnnkrmn0t8gvmo.jpg', 'https://images.unsplash.com/photo-1561693532-9ff59442a7db?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80'],
+    'characteristics': {'3347676':5, '3347677':2, '3347678':3, '3347679':1}
+  };
+
+  afterEach(async () => {
+    await pool.query('DELETE from photos where id > 2742540; \
+    DELETE from characteristicreview where id > 19327575; \
+    DELETE from reviews where id > 5774952;');
+  });
+
+  it('Should be able to add reviews', async () => {
+    const res = await req(server).post('/reviews').send(body);
+    expect(res.status).toEqual(201);
+  });
+
+  it('Should not add a review if the body is bad', async () => {
+    const res = await req(server).post('/reviews').send({'product_id': 'string'});
+    expect(res.status).toEqual(400);
+  });
+
+});
+
+describe('Get request for reviews/meta route', () => {
+  it('Should get the characteristics of product id', async () => {
+    const res = await req(server).get('/reviews/meta?product_id=1000011');
+    expect(res.body.product_id).toEqual('1000011');
   });
 });
